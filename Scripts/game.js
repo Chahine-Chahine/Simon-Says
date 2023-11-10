315f2e02197bed9a3c00caf5ac78bb2dcfbe212d
Chahine-Chahine <chahinesquare@hotmail.com>
2023-11-11 00:49:32 +0200

    targeting the userchoice and storing in array

--- Scripts/game.js
+++ Scripts/game.js
@@ -1,7 +1,7 @@
 // Global variables
 const buttons = ["green", "red", "yellow" , "blue"];
-const logicPattern = []
-const userPattern = []
+const logic_pattern = []
+const user_pattern = []
 
 
 
@@ -10,9 +10,14 @@ const userPattern = []
 let  sequenceCreator = ()=>{
 let random_number = Math.floor(Math.random() * 4);
 let random_color = buttons[random_number];
-logicPattern.push(random_color);
+logic_pattern.push(random_color);
 $("#" + random_color).fadeIn(100).fadeOut(100).fadeIn(100);
 }
 
+// selecting then storing the user choice 
+$(".btn").click(()=>{
+    // select the id attribute of the clicked button
+    let user_choice = $(".btn").attr("id");
+    user_pattern.push(user_choice);
+})
 
-sequenceCreator()
