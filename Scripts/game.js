12b8f1515b4f70c1de87ddcd20fe9399248e42d8
Chahine-Chahine <chahinesquare@hotmail.com>
2023-11-11 01:31:53 +0200

    adding buttonAnimation function and setting level counter

--- Scripts/game.js
+++ Scripts/game.js
@@ -3,7 +3,8 @@ const buttons = ["green", "red", "yellow" , "blue"];
 const logic_pattern = []
 const user_pattern = []
 
-
+// start at level 0
+let level = 0;
 
 
 // Arrow function to generate the game sequence creator
@@ -13,6 +14,9 @@ let random_color = buttons[random_number];
 logic_pattern.push(random_color);
 $("#" + random_color).fadeIn(100).fadeOut(100).fadeIn(100);
 playSound(random_color)
+level++
+$("#level-title").text("level" + " " + level);
+user_pattern = []
 }
 
 // selecting then storing the user choice 
@@ -22,6 +26,7 @@ $(".btn").click(()=>{
     user_pattern.push(user_choice);
     // add sound acc. to user color choice
     playSound(user_choice);
+    buttonAnimation(user_choice);
 
 })
 
@@ -30,4 +35,12 @@ $(".btn").click(()=>{
 let playSound = (sound) =>{
     let audio = new Audio("./sounds/" + sound + ".mp3");
     audio.play();
+}
+
+// function for button animation
+let buttonAnimation = (color_choice) =>{
+    $("#" + color_choice).addClass("pressed");
+    setTimeout(()=>{
+    $("#" + color_choice).removeClass("pressed");
+    },100);
 }
